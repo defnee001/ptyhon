ce831502ffd9edf090bb5f276093557986054816
defnee001 <119572295+defnee001@users.noreply.github.com>
2023-01-29 19:44:04 +0300

    Add files via upload

---
+++ deneme dosyası.py
@@ -0,0 +1,15 @@
+import modul1
+a=int(input("a:"))
+b=int(input("b:"))
+
+print(modul1.toplama(a,b))
+print(modul1.cikarma(a,b))
+print(modul1.carpma(a,b))
+print(modul1.bolme(a,b))
+print(modul1.us(a,b))
+print(modul1.karakok(a))
+print(modul1.derece(a))
+print(modul1.radyan(a))
+print(modul1.euler())
+print(modul1.pi())
+
---
+++ modul1.py
@@ -0,0 +1,26 @@
+def toplama(a,b):
+    return a+b
+def carpma(a,b):
+    return a*b
+def bolme(a,b):
+    return a/b
+def cikarma(a,b):
+    return a*b
+def us(a,b):
+    return a**b
+""" bu fonksiyonlar iki degeri dondurur"""
+
+def karakok(a):
+    return(a**0.5)
+def derece (a):
+    return a/180
+def radyan (a):
+    return 180/a
+""" bu fonksiyonlar bir değerin karşılığını bulur"""
+
+def euler():
+    return 7.39
+def pi():
+    return 3.14
+"""bu fonksiyonlar değer karşılıpını verir"""
+
